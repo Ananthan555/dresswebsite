5a1edd47d60cc91fe34d2655a0759c94749b5b47
Ananthan555 <ananthan.s5656@gmail.com>
2026-05-29 08:55:03 +0530

    about ediit

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -4,8 +4,8 @@ function Newsletter() {
       <div>
         <span>About</span>
         <h2>Visit our store</h2>
-        <p>Aruppukottai Min roadd, Bus stand</p>
-        <p>Kriyapatti - 626 106.</p>
+        <p>Aruppukottai Main road, Bus stand</p>
+        <p>Kariyapatti - 626 106.</p>
         <p>Mobile : 91506 48548, 8098954035</p>
       </div>
     </section>
